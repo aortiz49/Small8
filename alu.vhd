f3b22cbb813b6d51ab61cb82ce2502826676965e
Andy Ortiz <andyortiz93@gmail.com>
2014-05-13 14:38:01 -0400

    Add alu template

--- alu.vhd
+++ alu.vhd
@@ -18,8 +18,8 @@ architecture behavior of alu is
 begin
 
    process(A,D,C,sel)
-   variable temp_sum : signed(alu_width downto 0);               -- Add one more bit to find unsigned add carry
-   variable temp     : signed(alu_width-1 downto 0);             -- temp for final 
+   variable temp_sum : signed(alu_width downto 0);               -- Append bit to data width for sum w/carry operation
+   variable temp     : signed(alu_width-1 downto 0);             -- Temp var for final result 
   
 begin
 	c_out <= '0';
@@ -31,117 +31,41 @@ begin
 	case sel is
 ------------------------------------------------------------------------------------------------------
 		when F_ADCR =>
-			if(C = '1') then 
-				temp_sum := signed('0'&A)+signed('0'&D)+1;
-			else 
-				temp_sum := signed('0'&A)+signed('0'&D);
-			end if;
-			 
-				temp 		:= temp_sum(alu_width-1 downto 0);
-				c_out 	<= temp_sum(alu_width);                        
-				v_out 	<= temp_sum(alu_width) xor A(alu_width-1) xor D(alu_width-1)xor temp_sum(alu_width-1); 			
-				s_out 	<= temp(alu_width-1);
-				
-				if(temp = 0) then	
-					z_out <= '1';
-			   else	
-					z_out <= '0';
-				end if;
+		
 ------------------------------------------------------------------------------------------------------        
-      when F_SBCR =>
+       when F_SBCR =>
 			
-				temp_sum := signed('0'&A)+signed(not('0'&D))+1;
-				temp 	:= temp_sum(alu_width-1 downto 0);
-				c_out 	<= temp_sum(alu_width);                        
-				v_out 	<= temp_sum(alu_width) xor A(alu_width-1) xor D(alu_width-1)xor temp_sum(alu_width-1); 			
-				s_out 	<= temp(alu_width-1);
-
-				if(temp = 0) then	
-					z_out <= '1';
-				else	
-					z_out <= '0';
-				end if;
 ------------------------------------------------------------------------------------------------------ 			
 		when F_ANDR =>
-				temp := signed(A and D);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+			
 ------------------------------------------------------------------------------------------------------ 	
 		when F_ORR =>
-				temp := signed(A or D);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+				
 ------------------------------------------------------------------------------------------------------
 		when F_XORR =>
-				temp := signed(A xor D);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+				
 ------------------------------------------------------------------------------------------------------
 		when F_SLRL =>
-				temp 		:= SHIFT_LEFT(signed(A), 1);
-				temp(0)  := '0';
-				c_out <= A(alu_width-1);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+				
 ------------------------------------------------------------------------------------------------------
 	when F_SRRL =>
-				temp 		:= SHIFT_RIGHT(signed(A), 1);
-				temp(7)  := '0';
-				c_out <= A(0);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+				
 ------------------------------------------------------------------------------------------------------
 	when F_ROLC =>
-				temp 		:= SHIFT_LEFT(signed(A), 1);
-				temp(0)  := C;
-				c_out <= A(7);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+				
 ------------------------------------------------------------------------------------------------------			
-when F_RORC =>
-				temp 		:= SHIFT_RIGHT(signed(A), 1);
-				temp(7)  := C;
-				c_out <= A(0);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+     when F_RORC =>
+				
 ------------------------------------------------------------------------------------------------------	
 
- when others =>                                          -- Output is '0' and overflow is '0' for unused select values
+ when others =>                                         
             null;
 
 			     
     end case;
 	 
 	
-    output <= std_logic_vector(temp);
+    output <= std_logic_vector(temp);  -- Convert final value to std_logic_vector 
 	
   end process;
 end behavior;
