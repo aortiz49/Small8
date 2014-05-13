1c70025747270a41e77a2b26a23513f920d3fcf0
Andy Ortiz <andyortiz93@gmail.com>
2014-05-13 14:38:01 -0400

    Add alu template
    Update small8_lib.vhd
    
    Formatted proper spacing.
    
    added add and subtract instructions
    
    Update alu.vhd
    
    fix indentation issues on operation cases
    
    Update alu.vhd
    
    More indentation fixes on port information
    
    All operations writen

--- alu.vhd
+++ alu.vhd
@@ -5,12 +5,12 @@ use work.small8_lib.all;
 
 entity alu is 
 
-   port ( 
-		A,D 								: in std_logic_vector(alu_width-1 downto 0);	-- Accumulator reg and Data reg
-       	sel 								: in std_logic_vector(3 downto 0);	-- select line
-	   	C									: in std_logic;	-- Carry-in 
-	    c_out,v_out,s_out,z_out				: out std_logic;	-- status signals
-		output 								: out std_logic_vector(alu_width-1 downto 0)	--output 
+	port ( 
+		A,D 				: in std_logic_vector(alu_width-1 downto 0);	-- Accumulator reg and Data reg
+       		sel 				: in std_logic_vector(3 downto 0);	-- select line
+	   	C				: in std_logic;	-- Carry-in 
+	    	c_out,v_out,s_out,z_out		: out std_logic;	-- status signals
+		output 				: out std_logic_vector(alu_width-1 downto 0)	--output 
     ); 
 end alu; 
 
@@ -18,8 +18,8 @@ architecture behavior of alu is
 begin
 
    process(A,D,C,sel)
-   variable temp_sum : signed(alu_width downto 0);               -- Add one more bit to find unsigned add carry
-   variable temp     : signed(alu_width-1 downto 0);             -- temp for final 
+   variable temp_sum : signed(alu_width downto 0);               -- Append bit to data width for sum w/carry operation
+   variable temp     : signed(alu_width-1 downto 0);             -- Temp var for final result 
   
 begin
 	c_out <= '0';
@@ -44,63 +44,66 @@ begin
 				
 				if(temp = 0) then	
 					z_out <= '1';
-			   else	
+			   	else	
 					z_out <= '0';
 				end if;
 ------------------------------------------------------------------------------------------------------        
-      when F_SBCR =>
-			
-				temp_sum := signed('0'&A)+signed(not('0'&D))+1;
-				temp 	:= temp_sum(alu_width-1 downto 0);
-				c_out 	<= temp_sum(alu_width);                        
-				v_out 	<= temp_sum(alu_width) xor A(alu_width-1) xor D(alu_width-1)xor temp_sum(alu_width-1); 			
-				s_out 	<= temp(alu_width-1);
+      	when F_SBCR =>	
+			temp_sum := signed('0'&A)+signed(not('0'&D))+1;
+			temp 	:= temp_sum(alu_width-1 downto 0);
+			c_out 	<= temp_sum(alu_width);                        
+			v_out 	<= temp_sum(alu_width) xor A(alu_width-1) xor D(alu_width-1)xor temp_sum(alu_width-1); 			
+			s_out 	<= temp(alu_width-1);
 
-				if(temp = 0) then	
-					z_out <= '1';
-				else	
-					z_out <= '0';
-				end if;
+			if(temp = 0) then	
+				z_out <= '1';
+			else	
+				z_out <= '0';
+			end if;
 ------------------------------------------------------------------------------------------------------ 			
 		when F_ANDR =>
-				temp := signed(A and D);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+			temp := signed(A and D);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------ 	
 		when F_ORR =>
-				temp := signed(A or D);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+			temp := signed(A or D);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
 		when F_XORR =>
-				temp := signed(A xor D);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+			temp := signed(A xor D);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
 		when F_SLRL =>
-				temp 		:= SHIFT_LEFT(signed(A), 1);
-				temp(0)  := '0';
-				c_out <= A(alu_width-1);
-				if(temp = 0) then	
-					z_out <= '1';
-				else
-					z_out <= '0';
-				end if;
-				s_out 	<= temp(alu_width-1);
+			temp 		:= SHIFT_LEFT(signed(A), 1);
+			temp(0)  := '0';
+			c_out <= A(alu_width-1);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
-	when F_SRRL =>
+		when F_SRRL =>
 				temp 		:= SHIFT_RIGHT(signed(A), 1);
 				temp(7)  := '0';
 				c_out <= A(0);
@@ -111,7 +114,7 @@ begin
 				end if;
 				s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
-	when F_ROLC =>
+		when F_ROLC =>
 				temp 		:= SHIFT_LEFT(signed(A), 1);
 				temp(0)  := C;
 				c_out <= A(7);
@@ -122,7 +125,7 @@ begin
 				end if;
 				s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------			
-when F_RORC =>
+		when F_RORC =>
 				temp 		:= SHIFT_RIGHT(signed(A), 1);
 				temp(7)  := C;
 				c_out <= A(0);
@@ -134,8 +137,8 @@ when F_RORC =>
 				s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------	
 
- when others =>                                          -- Output is '0' and overflow is '0' for unused select values
-            null;
+ 		when others =>                                          -- Output is '0' and overflow is '0' for unused select values
+            temp := (others=>'0');                              -- takes care of inferred latch warning
 
 			     
     end case;
@@ -145,5 +148,4 @@ when F_RORC =>
 	
   end process;
 end behavior;
-
 
