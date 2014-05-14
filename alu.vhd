db5826f0ae4ec11ed1217c477aa976db4976a656
Andy Ortiz <andyortiz93@gmail.com>
2014-05-13 22:54:12 -0400

    All operations writen

--- alu.vhd
+++ alu.vhd
@@ -31,43 +31,121 @@ begin
 	case sel is
 ------------------------------------------------------------------------------------------------------
 		when F_ADCR =>
-		
+			if(C = '1') then 
+				temp_sum := signed('0'&A)+signed('0'&D)+1;
+			else 
+				temp_sum := signed('0'&A)+signed('0'&D);
+			end if;
+			 
+				temp 		:= temp_sum(alu_width-1 downto 0);
+				c_out 	<= temp_sum(alu_width);                        
+				v_out 	<= temp_sum(alu_width) xor A(alu_width-1) xor D(alu_width-1)xor temp_sum(alu_width-1); 			
+				s_out 	<= temp(alu_width-1);
+				
+				if(temp = 0) then	
+					z_out <= '1';
+			   	else	
+					z_out <= '0';
+				end if;
 ------------------------------------------------------------------------------------------------------        
-		when F_SBCR =>
-			
+      	when F_SBCR =>	
+			temp_sum := signed('0'&A)+signed(not('0'&D))+1;
+			temp 	:= temp_sum(alu_width-1 downto 0);
+			c_out 	<= temp_sum(alu_width);                        
+			v_out 	<= temp_sum(alu_width) xor A(alu_width-1) xor D(alu_width-1)xor temp_sum(alu_width-1); 			
+			s_out 	<= temp(alu_width-1);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else	
+				z_out <= '0';
+			end if;
 ------------------------------------------------------------------------------------------------------ 			
 		when F_ANDR =>
-			
+			temp := signed(A and D);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------ 	
 		when F_ORR =>
-				
+			temp := signed(A or D);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
 		when F_XORR =>
-				
+			temp := signed(A xor D);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
 		when F_SLRL =>
-				
+			temp 		:= SHIFT_LEFT(signed(A), 1);
+			temp(0)  := '0';
+			c_out <= A(alu_width-1);
+
+			if(temp = 0) then	
+				z_out <= '1';
+			else
+				z_out <= '0';
+			end if;
+			s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
 		when F_SRRL =>
-				
+				temp 		:= SHIFT_RIGHT(signed(A), 1);
+				temp(7)  := '0';
+				c_out <= A(0);
+				if(temp = 0) then	
+					z_out <= '1';
+				else
+					z_out <= '0';
+				end if;
+				s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------
 		when F_ROLC =>
-				
+				temp 		:= SHIFT_LEFT(signed(A), 1);
+				temp(0)  := C;
+				c_out <= A(7);
+				if(temp = 0) then	
+					z_out <= '1';
+				else
+					z_out <= '0';
+				end if;
+				s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------			
-    	when F_RORC =>
-				
+		when F_RORC =>
+				temp 		:= SHIFT_RIGHT(signed(A), 1);
+				temp(7)  := C;
+				c_out <= A(0);
+				if(temp = 0) then	
+					z_out <= '1';
+				else
+					z_out <= '0';
+				end if;
+				s_out 	<= temp(alu_width-1);
 ------------------------------------------------------------------------------------------------------	
 
- when others =>                                         
-            null;
+ 		when others =>                                          -- Output is '0' and overflow is '0' for unused select values
+            temp := (others=>'0');                              -- takes care of inferred latch warning
 
 			     
     end case;
 	 
 	
-    output <= std_logic_vector(temp);  -- Convert final value to std_logic_vector 
+    output <= std_logic_vector(temp);
 	
   end process;
 end behavior;
-
 
